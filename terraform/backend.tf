06f3a4d6d5b692cca90edb06a3a22325c3322b2d
kumar63086 <128176566+kumar63086@users.noreply.github.com>
2023-03-19 12:42:23 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
-    region = "us-east-1"
+    bucket = "kumar866"
+    region = "us-west-2"
     key = "eks/terraform.tfstate"
   }
 }
